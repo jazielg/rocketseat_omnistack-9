887ea0851e2613d78da251faafdf0eb7f782dbb1
jazielg <jazielgj1995@gmail.com>
2020-05-19 22:34:13 -0300

    add socket-io

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 // https://www.mongodb.com/cloud/atlas
 mongoose.connect('mongodb+srv://jaziel:PASSWORD@cluster0-wnuir.mongodb.net/semana09?retryWrites=true&w=majority', {
@@ -13,9 +18,28 @@ mongoose.connect('mongodb+srv://jaziel:PASSWORD@cluster0-wnuir.mongodb.net/seman
     useUnifiedTopology: true,
 });
 
+const connectedUsers = {}; // Utilizar Redis em produção
+
+io.on('connection', socket => {
+    // console.log('Usuario conectado', socket.id);
+    // socket.emit('hello', 'world');
+    // console.log(socket.handshake.query);
+
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
